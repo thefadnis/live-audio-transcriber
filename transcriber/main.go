25f9502fc4e5dbde3f7472cf4dd9e08e7595cab5
Aakarsh Fadnis <aakarsh.fadnis@colorado.edu>
2020-12-08 03:01:39 -0700

    code cleanup

--- transcriber/main.go
+++ transcriber/main.go
@@ -80,16 +80,12 @@ func main() {
 		InterimResults: true,
 	}
 
-	// If a port is defined, listen there for callbacks from leader election.
-	// Only send audio to Speech while we're the leader.
 	if *electionPort > 0 {
 		var ctx context.Context
 		var cancel context.CancelFunc
 		addr := fmt.Sprintf(":%d", *electionPort)
 		server := &http.Server{Addr: addr}
 
-		// Listen for interrupt. If so, cancel the context to stop transcriptions,
-		// then shutdown the HTTP server, which will end the main thread.
 		ch := make(chan os.Signal, 1)
 		signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
 		go func() {
@@ -105,8 +101,7 @@ func main() {
 
 		isLeader := false
 		webHandler := func(res http.ResponseWriter, req *http.Request) {
-			// If we're elected as leader, start a goroutine to read audio from
-			// Redis and stream to Cloud Speech
+
 			if strings.Contains(req.URL.Path, "start") {
 				if !isLeader {
 					isLeader = true
@@ -115,7 +110,6 @@ func main() {
 					go sendAudio(ctx, speechClient, streamingConfig)
 				}
 			}
-			// If we stop being the leader, stop sending audio
 			if strings.Contains(req.URL.Path, "stop") {
 				if isLeader {
 					isLeader = false
@@ -127,20 +121,14 @@ func main() {
 		}
 		http.HandleFunc("/", webHandler)
 		klog.Infof("Starting leader election listener at port %s", addr)
-		// blocks
 		server.ListenAndServe()
 	} else {
-		// Standalone execution, testing
 		klog.Info("Not doing leader election")
 		sendAudio(context.Background(), speechClient, streamingConfig)
 	}
 }
 
 // Consumes queued audio data from Redis, and sends to Speech.
-// Uses a reliable queueing approach such that the most recently received audio data
-// is also temporarily stored in a parallel 'recovery' queue.
-// See https://redis.io/commands/rpoplpush#pattern-reliable-queue.
-// At startup, or in the case of Redis errors, data in the recovery queue is replayed first.
 func sendAudio(ctx context.Context, speechClient *speech.Client, config speechpb.StreamingRecognitionConfig) {
 	var stream speechpb.Speech_StreamingRecognizeClient
 	receiveChan := make(chan bool)
@@ -178,8 +166,6 @@ func sendAudio(ctx context.Context, speechClient *speech.Client, config speechpb
 				emit("[REPLAY]")
 			}
 		} else {
-			// Blocking pop audio data off the queue, and push onto recovery queue.
-			// Timeout occasionally for hygiene.
 			result, err = redisClient.BRPopLPush(*audioQueue, *recoveryQueue, 5*time.Second).Result()
 			if err == redis.Nil { // pop timeout
 				continue
@@ -196,7 +182,6 @@ func sendAudio(ctx context.Context, speechClient *speech.Client, config speechpb
 			continue
 		}
 
-		// Establish bi-directional connection to Cloud Speech and
 		// start a go routine to listen for responses
 		if stream == nil {
 			stream = initStreamingRequest(ctx, speechClient, config)
@@ -223,10 +208,8 @@ func sendAudio(ctx context.Context, speechClient *speech.Client, config speechpb
 
 // Consumes StreamingResponses from Speech.
 func receiveResponses(stream speechpb.Speech_StreamingRecognizeClient, receiveChan chan bool) {
-	// Indicate that we're no longer listening for responses
 	defer close(receiveChan)
 
-	// If no results received from Speech for some period, emit any pending transcriptions
 	timer := time.NewTimer(*flushTimeout)
 	go func() {
 		<-timer.C
@@ -238,7 +221,6 @@ func receiveResponses(stream speechpb.Speech_StreamingRecognizeClient, receiveCh
 	for {
 		resp, err := stream.Recv()
 		if err != nil {
-			// Context cancelled - expected, e.g. stopped being leader
 			if status.Code(err) == codes.Canceled {
 				return
 			}
@@ -246,7 +228,6 @@ func receiveResponses(stream speechpb.Speech_StreamingRecognizeClient, receiveCh
 			return
 		}
 		if err := resp.Error; err != nil {
-			// Timeout - expected, when no audio sent for a time
 			if status.FromProto(err).Code() == codes.OutOfRange {
 				klog.Info("Timeout from API; closing connection")
 				return
@@ -258,20 +239,12 @@ func receiveResponses(stream speechpb.Speech_StreamingRecognizeClient, receiveCh
 		if !timer.Stop() {
 			return
 		}
-		// Ok, we have a valid response from Speech.
 		timer.Reset(*flushTimeout)
 		processResponses(*resp)
 	}
 }
 
 // Handles transcription results.
-// Broadly speaking, the goal is to emit 1-3 transcribed words at a time.
-// An index is maintained to determine last emitted words.
-// Emits a delimited string with 3 parts:
-// 'steady' - these words are assumed final (won't change)
-// 'pending' - interim transcriptions often evolve as Speech gets more data,
-// 		so the most recently transcribed words should be treated with caution
-// 'unstable' - low-stability transcription alternatives
 func processResponses(resp speechpb.StreamingRecognizeResponse) {
 	if len(resp.Results) == 0 {
 		return
@@ -282,7 +255,6 @@ func processResponses(resp speechpb.StreamingRecognizeResponse) {
 	elements := strings.Split(alternative.Transcript, " ")
 	length := len(elements)
 
-	// Speech will not further update this transcription; output it
 	if result.GetIsFinal() || alternative.GetConfidence() > 0 {
 		klog.Info("Final result! Resetting")
 		final := elements[lastIndex:]
@@ -296,13 +268,11 @@ func processResponses(resp speechpb.StreamingRecognizeResponse) {
 		return
 	}
 
-	// Unstable, speculative transcriptions (very likley to change)
 	unstable := ""
 	if len(resp.Results) > 1 {
 		unstable = resp.Results[1].Alternatives[0].Transcript
 	}
 	// Treat last N words as pending.
-	// Treat delta between last and current index as steady
 	if length < *pendingWordCount {
 		lastIndex = 0
 		pending = elements
@@ -320,7 +290,6 @@ func initStreamingRequest(ctx context.Context, client *speech.Client, config spe
 	if err != nil {
 		klog.Fatal(err)
 	}
-	// Send the initial configuration message.
 	if err := stream.Send(&speechpb.StreamingRecognizeRequest{
 		StreamingRequest: &speechpb.StreamingRecognizeRequest_StreamingConfig{
 			StreamingConfig: &config,
@@ -334,7 +303,6 @@ func initStreamingRequest(ctx context.Context, client *speech.Client, config spe
 }
 
 func emitStages(steady []string, pending []string, unstable string) {
-	// Concatenate the different segments, let client decide how to display
 	msg := fmt.Sprintf("%s|%s|%s", strings.Join(steady, " "),
 		strings.Join(pending, " "), unstable)
 	emit(msg)
