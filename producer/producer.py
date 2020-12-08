ce2692cbee19504b020917f83533fb6c377009de
abhasp <abhas0906@gmail.com>
2020-12-07 20:18:04 -0700

    Clean up producer

--- producer/producer.py
+++ producer/producer.py
@@ -3,22 +3,37 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 eventlet.monkey_patch()
 
+health_check_interval = 2
+# connected_count = 0
+q = queue.Queue()
+app = Flask(__name__)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--host', default='localhost')
 parser.add_argument('--port', default=8080)
 parser.add_argument('--redisHost', required=True)
 parser.add_argument('--redisQueue', default='transcriptions')
 parser.add_argument('--id', default='producer')
-args = parser.parse_args()
-health_check_interval = 2
-connected_count = 0
-buff = queue.Queue()
-app = Flask(__name__)
+params = parser.parse_args()
 
 socketio = SocketIO(ping_timeout=5, ping_interval=2)
-rdb = redis.Redis(host=args.redisHost, port=6379, db=0, socket_timeout=3,
+redisDB = redis.Redis(host=params.redisHost, port=6379, db=0, socket_timeout=3,
                   health_check_interval=health_check_interval)
 
+def _qread():
+    while True:
+        while not q.empty():
+            try:
+                transcript_part = redisDB.brpop(params.redisQueue, timeout=2)
+                if transcript_part is not None:
+                    socketio.emit('transcript', transcript_part[1].decode('utf-8'))
+            except redis.exceptions.ReadOnlyError as re:
+                print('Redis Read Error (failover?): %s' % re)
+                socketio.emit('transcript', '[REDIS-FAILOVER]')
+                socketio.sleep(health_check_interval)
+            except redis.exceptions.RedisError as err:
+                print('RedisError: %s' % err)
+        socketio.sleep(0.2)
 
 @app.route('/')
 def index():
@@ -27,35 +42,18 @@ def index():
 
 @socketio.on('connect')
 def connect():
-    print('Connected to %s' % args.id)
-    socketio.emit('pod_id', args.id)
+    print('Connected : %s' % params.id)
+    socketio.emit('pod_id', params.id)
     buff.put_nowait(1)
 
 
 @socketio.on('disconnect')
 def disconnect():
-    print('Disconnected from %s' % args.id)
+    print('Disconnected : %s' % params.id)
     buff.get_nowait()
 
-
-def _qread():
-    while True:
-        while not buff.empty():
-            try:
-                fragment = rdb.brpop(args.redisQueue, timeout=2)
-                if fragment is not None:
-                    socketio.emit('transcript', fragment[1].decode('utf-8'))
-            except redis.exceptions.ReadOnlyError as re:
-                print('Redis ReadOnlyError (failover?): %s' % re)
-                socketio.emit('transcript', '[REDIS-FAILOVER]')
-                socketio.sleep(health_check_interval)
-            except redis.exceptions.RedisError as err:
-                print('RedisError: %s' % err)
-        socketio.sleep(0.2)
-
-
 if __name__ == '__main__':
-    print('Starting %s...' % args.id)
+    print('Started %s...' % params.id)
     socketio.init_app(app)
     socketio.start_background_task(_qread)
-    socketio.run(app, host=args.host, port=args.port)
+    socketio.run(app, host=params.host, port=params.port)
